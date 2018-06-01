bad9633ad495c72d9275df7a640236cb1838584f
Alex <gunner14th@gmail.com>
2018-06-01 21:26:30 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Light IT Test</title>
+	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+
+	<script src="/assets/js/jquery.min.js"></script>
+	<script src="/assets/js/bootstrap.min.js"></script>
+
+	<script src="/assets/js/scripts.js"></script>
+</head>
+<body>
+	<div id="page">
+		<header>
+		    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+	      		<div class="container">
+		        	<span class="navbar-brand">LightIT Test</span>
+	    		</div>
+			</nav>
+	    </header>
+	    <main>
+			<div class="container">
+				<form>
+					<div class="form-group">
+						<textarea class="form-control" id="first" rows="8" placeholder="Input"></textarea>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control" id="first" rows="8" placeholder="Output" readonly></textarea>
+					</div>
+					<div class="form-group">
+						<button type="button" class="btn btn-dark">Primary</button>
+					</div>
+				</form>	
+			</div>
+	    </main>
+		<footer>
+			
+		</footer>
+	</div>
+</body>
+</html>
